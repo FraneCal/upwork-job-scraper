8598449af7dd2c491e777f57feb48004945acc77
Frane Ćalušić <90317417+FraneCal@users.noreply.github.com>
2024-10-01 11:23:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,10 +30,12 @@ class UpWorkJobScraper:
 
         options = Options()
         options.add_argument("--headless")
-        options.add_argument("--disable-gpu")  # Enable GPU support in headless mode
+        options.add_argument("--disable-gpu") 
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        options.add_argument(f'--user-agent={user_agent}')
+        options.add_argument("window-size=1920,1080")
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36")
+        #options.add_argument(f'--user-agent={user_agent}')
 
         driver = webdriver.Chrome(options=options)
         return driver
